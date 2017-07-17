87855f3a36d67aad6573548b1d6a05a8aa525dca
Luke Pierotti <lpierotti2@gmail.com>
2017-07-17 13:57:56 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/name' do
+		"My name is Luke"
+	end
+
+	get '/hometown' do
+		"My hometown is Scarsdale"
+	end
+
+	get '/favorite-song' do
+		"My favorite song is hello"
+	end
 end
